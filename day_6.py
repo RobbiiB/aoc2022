a317d840befbfaa131cb41190b160dcdfdf328a9
RobbiiB <119763215+RobbiiB@users.noreply.github.com>
2022-12-06 15:14:11 +0100

    Update and rename main.py to day_6.py

--- day_6.py
+++ day_6.py
@@ -1,6 +1,5 @@
-with open('/Users/robin/Input_day_6.txt', 'r') as file:
+with open('Input_day_6.txt', 'r') as file:
     datastream=file.read()
-abc=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 #part 1
 z=4
